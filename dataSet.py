4562a1e3ddb9c2e571963a081fd80d4dca9dd60e
xuhf <xuhf@fenbi.com>
2012-12-30 00:49:08 +0800

    ADD : dataSet.exportTables and test cases

--- dataSet.py
+++ dataSet.py
@@ -19,30 +19,61 @@ def _init():
     for line in lines:
         record = Record.deserialize(line)
         recordList.append(record)
-        uList = userSet.get(record.userId)
-        if uList is None:
-            uList = []
-            userSet[record.userId] = uList
-        uList.append(record)
-        iList = itemSet.get(record.itemId)
-        if iList is None:
-            iList = []
-            itemSet[record.itemId] = iList
-        iList.append(record)
 
 _init()
 
+def exportTables(records=recordList):
+    """ export item-user table and user-item table """
+    itemUsers = {}
+    userItems = {}
+    for record in records:
+        uList = itemUsers.get(record.itemId)
+        if uList is None:
+            uList = []
+            itemUsers[record.itemId] = uList
+        uList.append(record)
+        iList = userItems.get(record.userId)
+        if iList is None:
+            iList = []
+            userItems[record.userId] = iList
+        iList.append(record)
+    return itemUsers, itemUsers
+
+def _testExportTables():
+    itemUsers, userItems = exportTables(recordList)
+    count = 0
+    for records in itemUsers.values():
+        count += len(records)
+    if count != len(recordList):
+        print "Test Failed! len(recordList)=%d, but count itemUsers=%d" % (len(recordList), count)
+        return
+    count = 0
+    for records in userItems.values():
+        count += len(records)
+    if count != len(recordList):
+        print "Test Failed! len(recordList)=%d, but count userItems=%d" % (len(recordList), count)
+        return
+
+_SPLIT_COUNT = 8
 def randomSplit():
     train = []
     test = []
     for record in recordList:
-        if random.randint(0, 7)==0:
+        if random.randint(0, _SPLIT_COUNT-1)==0:
             test.append(record)
         else:
             train.append(record)
     return train, test
 
-if __name__ == '__main__':
+def _testRandomSplit():
     train, test = randomSplit()
-    print len(train)
-    print len(test)
+    if len(train) + len(test) != len(recordList):
+        print "TEST FAILED: len(train)=%d, len(test)=%d, but len(recordList)=%d" %(len(train), len(test)\
+            ,len(recordList))
+    ratio = 1.0*len(recordList)/len(test)
+    if ratio<_SPLIT_COUNT-0.5 or ratio>_SPLIT_COUNT+0.5:
+        print "TEST FAILED: ratio=%d" % ratio
+
+if __name__ == '__main__':
+    _testRandomSplit()
+    _testExportTables()
